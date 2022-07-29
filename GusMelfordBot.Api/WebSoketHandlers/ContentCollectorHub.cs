c0b4c74adadf76f68b8ad8b35f0a635162674bc4
Maksym Fedorenko <m.fedorenko@upswot.com>
2022-07-30 00:07:12 +0300

    [Add] SwitchVideo, PauseVideo

--- GusMelfordBot.Api/WebSoketHandlers/ContentCollectorHub.cs
+++ GusMelfordBot.Api/WebSoketHandlers/ContentCollectorHub.cs
@@ -4,8 +4,18 @@ namespace GusMelfordBot.Api.WebSoketHandlers;
 
 public class ContentCollectorHub : Hub
 {
-    public async Task Send(string message)
+    public async Task ChangeVideoTime(double currentTime)
     {
-        await Clients.All.SendAsync("Send", message);
+        await Clients.Others.SendAsync("ChangeVideoTime", currentTime);
+    }
+    
+    public async Task SwitchVideo(string direction)
+    {
+        await Clients.All.SendAsync("SwitchVideo", direction);
+    }
+    
+    public async Task PauseVideo(bool isPaused)
+    {
+        await Clients.All.SendAsync("PauseVideo", isPaused);
     }
 }
