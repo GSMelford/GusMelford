fb550dc49381f210de42bc35ecf10523a66f6bdc
Maksym Fedorenko <m.fedorenko@upswot.com>
2022-09-30 14:16:32 +0300

    [Fixed] Forbidden after information request

--- ContentCollector.MicroService/Services/ContentProviders/TikTok/TikTokContentHandlers/VideoInformationHandler.cs
+++ ContentCollector.MicroService/Services/ContentProviders/TikTok/TikTokContentHandlers/VideoInformationHandler.cs
@@ -1,61 +1,34 @@
-﻿using ContentCollector.Domain.ContentProviders;
+﻿using System.Text.RegularExpressions;
+using ContentCollector.Domain.ContentProviders;
 using ContentCollector.Services.ContentProviders.TikTok.TikTokContentHandlers.Abstractions;
 using GusMelfordBot.Extensions;
-using Newtonsoft.Json.Linq;
 using RestSharp;
 
 namespace ContentCollector.Services.ContentProviders.TikTok.TikTokContentHandlers;
 
 public class VideoInformationHandler : AbstractTikTokContentHandler
 {
+    private static readonly Regex RegexGetDownloadLink = new ("\"downloadAddr\":\"(\\S+?)\"", RegexOptions.Compiled);
+    private static readonly Regex RegexHeight = new ("\"height\":(\\d*)", RegexOptions.Compiled);
+    private static readonly Regex RegexWidth = new ("\"width\":(\\d*)", RegexOptions.Compiled);
+    private static readonly Regex RegexDuration = new ("\"duration\":(\\d*)", RegexOptions.Compiled);
+    
     public override async Task<ProcessedTikTokContent?> Handle(ProcessedTikTokContent processedTikTokContent)
     {
-        JToken? token = await GetVideoInformation(processedTikTokContent.OriginalLink);
-        if (token is null)
+        RestRequest restRequest = new RestRequest(processedTikTokContent.OriginalLink) { Timeout = 60000 };
+        restRequest.AddHeader("User-Agent", Constants.UserAgent);
+        string content = (await new RestClient().ExecuteAsync(restRequest)).Content!;
+        
+        if (string.IsNullOrEmpty(content))
         {
             return processedTikTokContent;
         }
-
-        int.TryParse(token["statusCode"]?.ToString(), out int code);
-        processedTikTokContent.VideoStatusCode = code;
-        processedTikTokContent.DownloadLink = GetDownloadLink(token);
-        processedTikTokContent.Height = GetHeightContent(token);
-        processedTikTokContent.Width = GetWidthContent(token);
-        processedTikTokContent.Duration = GetDurationContent(token);
+        
+        processedTikTokContent.VideoStatusCode = 0;
+        processedTikTokContent.DownloadLink = Regex.Unescape(RegexGetDownloadLink.Match(content).Groups[1].Value);
+        processedTikTokContent.Height = RegexHeight.Match(content).Groups[1].Value.ToInt();
+        processedTikTokContent.Width = RegexWidth.Match(content).Groups[1].Value.ToInt();
+        processedTikTokContent.Duration = RegexDuration.Match(content).Groups[1].Value.ToInt();
         return await base.Handle(processedTikTokContent);
-    }
-    
-    private async Task<JToken?> GetVideoInformation(string refererLink)
-    {
-        string requestUrl = refererLink.BuildVideoInformationUrl();
-        RestRequest restRequest = new RestRequest(requestUrl) { Timeout = 60000 };
-        restRequest.AddHeader("User-Agent", Constants.UserAgent);
-
-        try {
-            return JToken.Parse((await new RestClient().ExecuteAsync(restRequest)).Content!);
-        }
-        catch {
-            return null;
-        }
-    }
-    
-    private static string? GetDownloadLink(JToken videoInformation)
-    {
-        return videoInformation["itemInfo"]?["itemStruct"]?["video"]?["downloadAddr"]?.ToString();
-    }
-    
-    private static int? GetHeightContent(JToken videoInformation)
-    {
-        return videoInformation["itemInfo"]?["itemStruct"]?["video"]?["height"]?.ToString().ToInt();
-    }
-    
-    private static int? GetWidthContent(JToken videoInformation)
-    {
-        return videoInformation["itemInfo"]?["itemStruct"]?["video"]?["width"]?.ToString().ToInt();
-    }
-    
-    private static int? GetDurationContent(JToken videoInformation)
-    {
-        return videoInformation["itemInfo"]?["itemStruct"]?["video"]?["duration"]?.ToString().ToInt();
     }
 }
